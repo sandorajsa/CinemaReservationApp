f33181a39ccb9df2de82c583f0bad8544bf2bc91
dtzso <zsoldos.donat.dominik@students.jedlik.eu>
2025-04-21 16:54:02 +0200

    Fájl olvasás, classok, kategória gombok dinamikusan

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,17 +11,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.Json;
 
 namespace CinemaReservationApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public ObservableCollection<Movie> Movies { get; set; }
+        private ObservableCollection<Movie> moviesToShow;
+
+        public ObservableCollection<Movie> MoviesToShow
+        {
+            get { return moviesToShow; }
+            set { moviesToShow = value;OnPropertyChanged(nameof(MoviesToShow)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string tulajdonsagNev)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tulajdonsagNev));
+        }
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            FileRead();
+            CatButtons();
+        }
+        private void FileRead()
+        {
+            string jsonStr = File.ReadAllText("./Resources/cinema_movies.json");
+            Movies = JsonSerializer.Deserialize<ObservableCollection<Movie>>(jsonStr);
+            MoviesToShow = JsonSerializer.Deserialize<ObservableCollection<Movie>>(jsonStr);
+
+        }
+        private void CatButtons()
+        {
+            StackPanel stp = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+            foreach (var movie in Movies)
+            {
+                Button btn = new Button()
+                {
+                    Content = movie.genre.ToUpper(),
+                    Style = (Style)FindResource("categoryButtons") 
+                };
+                stp.Children.Add(btn);
+            }
+            search_GR.Children.Add(stp);
         }
     }
 }
---
+++ Movies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaReservationApp
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string poster { get; set; }
+        public string[] play_dates { get; set; }
+        public int length_minutes { get; set; }
+        public string age_limit { get; set; }
+        public string genre { get; set; }
+    }
+
+}
